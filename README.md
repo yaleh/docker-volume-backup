dbb01889724e7953eca03d0615668074ec7b87ff
cYale Huang <calvino.huang@gmail.com>
2022-11-04 11:30:42 +0000

    Added network backup/restore.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Docker Volume Backup
 
-Docker Volume Backup is a simple tool to backup and restore Docker volumes for containers and composes.
-It requires no access to docker-compose file, and is compatible with Portainer and Docker Compose.
+Docker Volume Backup is a simple tool to backup and restore Docker volumes and attachable networks
+for containers and composes. It requires no access to docker-compose file, and is compatible with
+Portainer and Docker Compose.
 
 ## Installation
 
@@ -17,16 +18,36 @@ pip3 install -r requirements.txt
 
 To backup a volume, run the following command:
 
-    docker-volume-backup.py -v <volumes> -d <backup_dir>
+    docker-volume-backup.py -v <volume> -d <backup_dir>
 
-To backup volumes of a container, run the following command:
+To backup multiple volumes, use a comma-separated list:
 
-    docker-volume-backup.py -c <containers> -d <backup_dir>
+    docker-volume-backup.py -v <volume1>,<volume2>,<volume3> -d <backup_dir>
 
-To backup volumes of a compose, run the following command:
+To backup a network, run the following command:
+
+    docker-volume-backup.py -n <network> -d <backup_dir>
+
+To backup multiple networks, use a comma-separated list:
+
+    docker-volume-backup.py -n <network1>,<network2>,<network3> -d <backup_dir>
+
+To backup volumes and attachable networks of a container, run the following command:
+
+    docker-volume-backup.py -c <container> -d <backup_dir>
+
+To backup volumes and attachable networks of multiple containers, use a comma-separated list:
+
+    docker-volume-backup.py -c <container1>,<container2>,<container3> -d <backup_dir>
+
+To backup volumes and attachable networks of a compose, run the following command:
     
-    docker-volume-backup.py -m <composes> -d <backup_dir>
+    docker-volume-backup.py -m <compose> -d <backup_dir>
 
+To backup volumes and attachable networks of multiple composes, use a comma-separated list:
+
+    docker-volume-backup.py -m <compose1>,<compose2>,<compose3> -d <backup_dir>
+    
 ### Restore
 
 To restore volume(s), run the following command:
@@ -36,12 +57,15 @@ To restore volume(s), run the following command:
 ### Options
 
     -v, --volumes <volumes>          Volumes to backup, separated by comma
+    -n, --networks <networks>        Networks to backup, separated by comma
     -c, --containers <containers>    Containers to backup, separated by comma
     -m, --composes <composes>        Composes to backup, separated by comma
     -d, --destination <destination>  Destination to backup to
     --all-volumes                    Backup all volumes
-    --all-containers                 Backup all containers
-    --all-composes                   Backup all composes
+    --all-containers                 Backup volumes of all containers
+    --all-composes                   Backup volumes of all composes
+    -nv, --no-volumes                Do not backup volumes
+    -nn, --no-networks               Do not backup networks
     -r, --restore                    Restore from backup
     -l, --list                       List volumes only, don't backup
     -h, --help                       Display help for command
--- docker-volume-backup.py
+++ docker-volume-backup.py
@@ -50,9 +50,56 @@ def get_volumes(volume_names):
 
     return volumes
 
+def get_networks(network_names):
+    networks = []
+    for network_name in network_names:
+        networks.append(client.networks.get(network_name))
+
+    return networks
+
+def get_all_networks():
+    networks = client.networks.list()
+    return networks
+
+def get_networks_by_containers(containers):
+    networks = []
+    for container in containers:
+        network_names = list(container.attrs['NetworkSettings']['Networks'].keys())
+        for network_name in network_names:
+            network = client.networks.get(network_name)
+            # only backup attachable bridge networks
+            if network.attrs['Driver'] == 'bridge' and network.attrs['Attachable']:
+                networks.append(network)
+
+    return networks
+
+def backup_networks(networks, backup_dir):
+    for n in networks:
+        # backup attachable bridge networks only
+        if n.attrs['Driver'] == 'bridge' and n.attrs['Attachable']:
+            print("Backing up network {}".format(n.name))
+            os.system("docker network inspect {} > {}".format(n.name, os.path.join(backup_dir, "{}.json".format(n.name))))
+
+def restore_networks(backup_dir):
+    for f in os.listdir(backup_dir):
+        if f.endswith(".json"):
+            network_name = f.replace(".json", "")
+            print("Restoring network {}".format(network_name))
+            network_json = os.path.join(backup_dir, f)
+            network_specs = json.load(open(network_json))
+            if network_specs['Driver'] == 'bridge' and network_specs['Attachable']:
+                os.system("docker network create --driver bridge --attachable {}".format(network_name))
+                os.system("docker network create --driver bridge --subnet {} --gateway {} {}".format(network_specs['IPAM']['Config'][0]['Subnet'], network_specs['IPAM']['Config'][0]['Gateway'], network_name))
+
 def print_volumes(volumes):
+    print("\nVolumes:\n")
     for v in volumes:
         print(v.name)
+
+def print_networks(networks):
+    print("\nNetworks:\n")
+    for n in networks:
+        print(n.name)
 
 def backup_volumes(volumes, backup_dir):
     for v in volumes:
@@ -72,20 +119,32 @@ def generate_backup_checksums(volumes, backup_dir):
         files.append(os.path.join(backup_dir, "{}.tar.gz".format(v.name)))
     os.system("sha256sum {} > {}".format(" ".join(files), os.path.join(backup_dir, "checksums.sha256")))
 
-def generate_backup_report(volumes, backup_dir):
-    report = []
+def generate_backup_report(volumes, networks, backup_dir):
+    volume_report = []
     for v in volumes:
-        report.append("{}: {} {}".format(
+        volume_report.append("{}: {} {}".format(
                                             v.name,
                                             os.path.join(backup_dir, "{}.tar.gz".format(v.name)), 
                                             os.path.getsize(os.path.join(backup_dir, "{}.tar.gz".format(v.name)))
                                         ))
 
+    network_report = []
+    for n in networks:
+        network_report.append("{}: {} {}".format(
+                                            n.name,
+                                            os.path.join(backup_dir, "{}.json".format(n.name)), 
+                                            os.path.getsize(os.path.join(backup_dir, "{}.json".format(n.name)))
+                                        ))
+
     with open(os.path.join(backup_dir, "backup_report.txt"), "w") as f:
         f.write("Docker Volume Backup\n")
         f.write("{}\n".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-        f.write("{}\n\n".format("=" * 20))
-        f.write("\n\n".join(report))
+        f.write("{}\n".format("=" * 20))
+        f.write("\nVolumes:\n\n")
+        f.write("\n".join(volume_report))
+        f.write("\n")
+        f.write("\nNetworks:\n\n")
+        f.write("\n".join(network_report))
         f.write("\n")
 
 def verify_backup_checksums(backup_dir):
@@ -118,15 +177,20 @@ def main():
     parser.add_argument('--all-containers', action='store_true', help='backup all containers')
     parser.add_argument('--composes', '-m', help='composes to backup')
     parser.add_argument('--all-composes', action='store_true', help='backup all composes')
+    parser.add_argument('--networks', '-n', help='networks to backup')
+    parser.add_argument('--all-networks', action='store_true', help='backup all attachable networks')
     parser.add_argument('--backup-dir', '-d', help='backup directory')
     parser.add_argument('--restore', '-r', help='restore volumes', action='store_true')
-    parser.add_argument('--list', '-l', action='store_true', help='list volumes only')
+    parser.add_argument('--no-volume', '-nv', help='do not backup/restore volumes', action='store_true')
+    parser.add_argument('--no-network', '-nn', help='do not backup/restore networks', action='store_true')
+    parser.add_argument('--list', '-l', action='store_true', help='list volumes and networks only')
     args = parser.parse_args()
 
     global client
     client = docker.from_env()
 
     volume_names = args.volumes.split(',') if args.volumes else []
+    network_names = args.networks.split(',') if args.networks else []
     container_names = args.containers.split(',') if args.containers else []
     compose_names = get_all_compose_names() if args.all_composes else args.composes.split(',') if args.composes else []
     backup_dir = args.backup_dir if args.backup_dir else os.getcwd()
@@ -136,22 +200,39 @@ def main():
     containers = [*set(containers)]
     volumes = get_all_volumes() if args.all_volumes else (get_volumes(volume_names) + get_volumes_from_containers(containers))
     volumes = [*set(volumes)]
+    networks = get_all_networks() if args.all_networks else get_networks(network_names)+get_networks_by_containers(containers)
+    networks = [*set(networks)]
 
     if args.list:
         print_volumes(volumes)
+        print_networks(networks)
         exit(0)
 
     if backup_dir is None:
         print("Backup directory not specified")
         exit(1)
 
+    # get absolute path of backup directory
+    backup_dir = os.path.abspath(backup_dir)
+
+    if args.no_volume:
+        volumes = []
+    if args.no_network:
+        networks = []
+
     if args.restore:
-        verify_backup_checksums(backup_dir)
-        restore_volumes(backup_dir)
+        if not args.no_volume:
+            verify_backup_checksums(backup_dir)
+            restore_volumes(backup_dir)
+        if not args.no_network:
+            restore_networks(backup_dir)
     else:
-        backup_volumes(volumes, backup_dir)
-        generate_backup_checksums(volumes, backup_dir)
-        generate_backup_report(volumes, backup_dir)
+        if not args.no_volume:
+            backup_volumes(volumes, backup_dir)
+            generate_backup_checksums(volumes, backup_dir)
+        if not args.no_network:
+            backup_networks(networks, backup_dir)
+        generate_backup_report(volumes, networks, backup_dir)
         
 if __name__ == '__main__':
     main()
